ea82578501749ea63f5b04438c2d64b7475ecf6a
wooti <wooti@users.noreply.github.com>
2015-07-24 20:57:51 +0100

    Removed unused setters

--- src/main/java/com/wouterbreukink/onedrive/client/resources/facets/Quota.java
+++ src/main/java/com/wouterbreukink/onedrive/client/resources/facets/Quota.java
@@ -16,31 +16,15 @@ public class Quota {
         return used;
     }
 
-    public void setUsed(long used) {
-        this.used = used;
-    }
-
     public long getRemaining() {
         return remaining;
-    }
-
-    public void setRemaining(long remaining) {
-        this.remaining = remaining;
     }
 
     public long getDeleted() {
         return deleted;
     }
 
-    public void setDeleted(long deleted) {
-        this.deleted = deleted;
-    }
-
     public String getState() {
         return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
     }
 }
