4aef9e531d2c8c99bc202e04bc9935a3846cc7b2
Federico Cavalieri <f@28.io>
2015-08-17 18:44:46 +0200

    Refactoring UploadSession

---
+++ src/main/java/com/wouterbreukink/onedrive/client/OneDriveUploadSessionBase.java
@@ -0,0 +1,101 @@
+package com.wouterbreukink.onedrive.client;
+
+import java.io.File;
+import java.io.IOException;
+
+public abstract class OneDriveUploadSessionBase 
+{
+    // Upload in chunks of 5MB as per MS recommendation
+    protected static final int CHUNK_SIZE = 5 * 1024 * 1024;
+    protected final File file;
+    protected final String remoteFilename;
+    protected final String uploadUrl;
+    protected OneDriveItem parent;
+    protected Range[] ranges;
+    protected long totalUploaded;
+    protected long lastUploaded;
+    protected OneDriveItem item;
+    
+    protected OneDriveUploadSessionBase(OneDriveItem parent, File file, String remoteFilename, String uploadUrl, String[] ranges) 
+    {
+        this.parent = parent;
+        this.file = file;
+        this.remoteFilename = remoteFilename;
+        this.uploadUrl = uploadUrl;
+        setRanges(ranges);
+    }
+    
+    public void setRanges(String[] stringRanges) 
+    {
+        this.ranges = new Range[stringRanges.length];
+        for (int i = 0; i < stringRanges.length; i++) {
+            long start = Long.parseLong(stringRanges[i].substring(0, stringRanges[i].indexOf('-')));
+
+            String s = stringRanges[i].substring(stringRanges[i].indexOf('-') + 1);
+
+            long end = 0;
+            if (!s.isEmpty()) {
+                end = Long.parseLong(s);
+            }
+
+            ranges[i] = new Range(start, end);
+        }
+
+        if (ranges.length > 0) {
+            lastUploaded = ranges[0].start - totalUploaded;
+            totalUploaded = ranges[0].start;
+        }
+    }
+
+
+    
+    
+	abstract byte[] getChunk() throws IOException;
+
+	public long getTotalUploaded() {
+        return totalUploaded;
+    }
+
+    public long getLastUploaded() {
+        return lastUploaded;
+    }
+
+    public OneDriveItem getParent() {
+        return parent;
+    }
+
+    public String getUploadUrl() {
+        return uploadUrl;
+    }
+
+    public File getFile() {
+        return file;
+    }
+    
+    public String getRemoteFilename() {
+        return remoteFilename;
+    }
+
+    public boolean isComplete() {
+        return item != null;
+    }
+
+	abstract void setComplete(OneDriveItem item);
+
+	abstract OneDriveItem getItem();
+
+	abstract long getRemoteFileLength();
+	
+    private static class Range 
+    {
+        public long start;
+        @SuppressWarnings("unused")
+		public long end;
+
+        private Range(long start, long end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+}
